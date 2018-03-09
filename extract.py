e28095bdea88988b137e6385a9a01c194439b494
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-03-08 18:42:57 -0700

    extract data out of file

---
+++ extract.py
@@ -0,0 +1,13 @@
+def main():
+    with open('favorites.txt') as f:
+        for line in f:
+            line = line.strip()
+            spl = line.split('|')
+            t = spl[2]
+            title = t[1:t.index('(')-1]
+            link = t[t.index('(')+1: t.index(')')]
+            author = spl[3]
+            print(title, link, author)
+
+if __name__ == '__main__':
+    main()
